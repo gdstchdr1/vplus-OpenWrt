abd9a7524fe27a4021b044fd09248d16a0fb91d2
gdstchdr1 <72061401+gdstchdr1@users.noreply.github.com>
2024-03-07 11:44:21 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -19,3 +19,14 @@ sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/conf
 git clone https://github.com/ophub/luci-app-amlogic.git  package-temp/luci-app-amlogic
 mv -f package-temp/luci-app-amlogic/luci-app-amlogic package/lean/
 rm -rf package-temp
+
+# Add luci-app-unblockneteasemusic
+git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git  package-temp/luci-app-unblockneteasemusic
+mv -f package-temp/luci-app-unblockneteasemusic package/lean/
+rm -rf package-temp
+
+# Modify default IP  修改后台登陆IP
+sed -i 's/192.168.1.1/192.168.5.8/g' package/base-files/files/bin/config_generate
+
+# 修改概览里时间显示为中文数字
+sed -i 's/os.date()/os.date("%Y年%m月%d日") .. " " .. translate(os.date("%A")) .. " " .. os.date("%X")/g' package/lean/autocore/files/arm/index.htm
